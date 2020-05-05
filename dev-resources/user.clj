563737f371ce5cbcdc90abb7ad441514b82691ec
Brian Brunner <brian.brunner.bb@gmail.com>
2020-05-05 13:31:29 -0600

    Add component based config

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -2,10 +2,11 @@
   (:require
    [wfs.schema :as s]
    [com.walmartlabs.lacinia :as lacinia]
+   [com.walmartlabs.lacinia.pedestal :as lp]
+   [io.pedestal.http :as http]
+   [clojure.java.browse :refer [browse-url]]
    [clojure.walk :as walk])
   (:import (clojure.lang IPersistentMap)))
-
-(def schema (s/load-schema))
 
 (defn simplify
   "Converts all ordered maps nested within the map into standard hash maps, and
@@ -24,7 +25,23 @@
        node))
    m))
 
+(defonce system (system/new-system))
+
 (defn q
   [query-string]
-  (-> (lacinia/execute schema query-string nil nil)
+  (-> system
+      :schema-provider
+      :schema
+      (lacinia/execute query-string nil nil)
       simplify))
+
+(defn start
+  []
+  (alter-var-root #'system component/start-system)
+  (browse-url "http://localhost:8888/")
+  :started)
+
+(defn stop
+  []
+  (alter-var-root #'system component/stop-system)
+  :stopped)
--- src/wfs/schema.clj
+++ src/wfs/schema.clj
@@ -4,6 +4,7 @@
    [clojure.java.io :as io]
    [com.walmartlabs.lacinia.util :as util]
    [com.walmartlabs.lacinia.schema :as schema]
+   [com.stuartsierra.component :as component]
    [clojure.edn :as edn]))
 
 (defn resolve-recipe-by-id
@@ -12,6 +13,7 @@
     (recipes-map id)))
 
 (defn resolver-map []
+  [component]
   (let [dummy-data (-> (io/resource "dummy-data.edn")
                        slurp
                        edn/read-string)
@@ -21,8 +23,23 @@
     {:query/recipe-by-id (partial resolve-recipe-by-id recipes-map)}))
 
 (defn load-schema []
+  [component]
   (-> (io/resource "wfs-schema.edn")
       slurp
       edn/read-string
-      (util/attach-resolvers (resolver-map))
+      (util/attach-resolvers (resolver-map component))
       schema/compile))
+
+(defrecord SchemaProvider [schema]
+
+  component/Lifecycle
+
+  (start [self]
+    (assoc self :schema (load-schema self)))
+
+  (stop [self]
+    (assoc self :schema nil)))
+
+(defn new-schema-provider
+  []
+  {:schema-provider (map->SchemaProvider {})})
