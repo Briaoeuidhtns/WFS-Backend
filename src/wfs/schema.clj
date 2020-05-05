dc978fe9ad6070f77521f65c6fb89b176227d630
Brian Brunner <brian.brunner.bb@gmail.com>
2020-05-05 10:34:55 -0600

    Add dummy data, trivial queries by id

---
+++ src/wfs/schema.clj
@@ -0,0 +1,28 @@
+(ns wfs.schema
+  "Contains custom resolvers and a function to provide the full schema."
+  (:require
+   [clojure.java.io :as io]
+   [com.walmartlabs.lacinia.util :as util]
+   [com.walmartlabs.lacinia.schema :as schema]
+   [clojure.edn :as edn]))
+
+(defn resolve-recipe-by-id
+  [recipes-map context args value]
+  (let [{:keys [id]} args]
+    (recipes-map id)))
+
+(defn resolver-map []
+  (let [dummy-data (-> (io/resource "dummy-data.edn")
+                       slurp
+                       edn/read-string)
+        recipes-map (->> dummy-data
+                         :recipes
+                         (reduce #(assoc %1 (:id %2) %2) {}))]
+    {:query/recipe-by-id (partial resolve-recipe-by-id recipes-map)}))
+
+(defn load-schema []
+  (-> (io/resource "wfs-schema.edn")
+      slurp
+      edn/read-string
+      (util/attach-resolvers (resolver-map))
+      schema/compile))
