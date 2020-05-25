7ba05e225af6d2bb8b02a8d2564eb7f93c2a085e
Brian Brunner <brian.brunner.bb@gmail.com>
2020-05-25 17:02:18 -0600

    Add identity into app context and logs

--- src/wfs/auth/interceptor.clj
+++ src/wfs/auth/interceptor.clj
@@ -1,15 +1,20 @@
 (ns wfs.auth.interceptor
   (:require
-   [io.pedestal.interceptor.helpers :refer [defbefore]]
    [buddy.auth.backends.token :refer [jws-backend]]
    [buddy.auth.middleware :as middleware]
+   [io.pedestal.interceptor.helpers :refer [defbefore]]
+   [taoensso.timbre :as log]
    [wfs.auth.user :as auth]))
 
-(def auth-backend
+(def ^:private auth-backend
   (jws-backend
     {:secret auth/pubkey :token-name "Bearer" :options {:alg :eddsa}}))
 
 (defbefore
   user-info
-  [context]
-  (update context :request middleware/authentication-request auth-backend))
+  [{:keys [request] :as context}]
+  (let [ident (middleware/authenticate-request request [auth-backend])]
+    (log/info "Claim validated as" identity)
+    (assoc-in context
+      [:request :lacinia-app-context ::auth/identity]
+      ident)))
