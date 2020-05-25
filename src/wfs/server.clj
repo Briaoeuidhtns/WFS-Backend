ab2b5075dc2e45b9e7d9cf44075805da579d0144
Brian Brunner <brian.brunner.bb@gmail.com>
2020-05-25 17:06:36 -0600

    Add interceptor to log errors

--- src/wfs/server.clj
+++ src/wfs/server.clj
@@ -3,16 +3,24 @@
    [com.stuartsierra.component :as component]
    [com.walmartlabs.lacinia.pedestal :as lp]
    [io.pedestal.http :as http]
+   [io.pedestal.interceptor.chain :as chain]
    [wfs.auth.interceptor :as auth.interceptors]
    [taoensso.timbre :as log]))
 
 (defn- service-map
   [schema options]
   (let [opt          (merge options {})
-        interceptors (-> (lp/default-interceptors schema opt)
-                         (lp/inject auth.interceptors/user-info
-                                    :after
-                                    ::lp/inject-app-context))]
+        interceptors
+        (-> (lp/default-interceptors schema opt)
+            (lp/inject auth.interceptors/user-info
+                       :after
+                       ::lp/inject-app-context)
+            (lp/inject {:name ::spy
+                        :error (fn [context error]
+                                 (log/error error)
+                                 (assoc context ::chain/error error))}
+                       :before
+                       ::auth.interceptors/user-info))]
     (lp/service-map schema (assoc opt :interceptors interceptors))))
 
 (defrecord Server [schema-provider server]
